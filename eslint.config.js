e6f5cf36e5e459c18d9bf2553b956335c730cf75
Nicolas Herrera <calamitoso@gmail.com>
2026-06-02 14:23:13 -0400

    chore: prepare focus timer project shell

--- eslint.config.js
+++ eslint.config.js
@@ -27,4 +27,10 @@ export default [
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
+  {
+    files: ['src/test/**/*.{js,jsx}'],
+    languageOptions: {
+      globals: globals.vitest,
+    },
+  },
 ]
